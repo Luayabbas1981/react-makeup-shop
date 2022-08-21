63d9208f9688971a2cf78a24d0aa73fa843c0fa5
Luayabbas1981 <abbasluay1981@gmail.com>
2022-08-22 01:18:39 +0200

    fix-everything

--- src/Components/ProductDetails.js
+++ src/Components/ProductDetails.js
@@ -34,7 +34,7 @@ function ProductDetails() {
         <div className="product">
           <Card
             className="product-card"
-            style={{ width: "18rem", height: "30rem" }}
+            style={{ width: "16rem", height: "25rem" }}
           >
             <Card.Img
               className="card-photo"
